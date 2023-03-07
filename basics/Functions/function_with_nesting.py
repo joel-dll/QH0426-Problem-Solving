2acd01df58c009799420287d0ddc8f67c065e801
claudiamendes <1ribev81@solent.ac.uk>
2023-03-07 11:21:33 +0000

    Added code to demonstrate the use of a user-defined function with a nested decision.

---
+++ basics/Functions/function_with_nesting.py
@@ -0,0 +1,11 @@
+
+
+def beep_boop_escape():
+    user = input("What lies before us?")
+    if user == "a large boulder":
+        print("It's time to run!")
+
+    else:
+        print("We will be fine!")
+
+beep_boop_escape()
