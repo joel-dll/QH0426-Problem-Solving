964c522b388476881a4e1803e8112781f3b86ff8
claudiamendes <1ribev81@solent.ac.uk>
2023-03-28 11:30:55 +0100

    Added code to display information about the current working directory.

---
+++ basics/Data/files/cwd.py
@@ -0,0 +1,17 @@
+import os
+
+
+def cwd():
+    path = os.getcwd()
+    print(f"The current working directory is {path}")
+    print("The directory contains the following: ")
+    for file in os.listdir(path):
+        print (file)
+    return path
+
+
+def run():
+    print("Processing...")
+    cwd()
+
+run()
